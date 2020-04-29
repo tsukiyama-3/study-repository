cc560b9dba55c6855d6adf439054e3ddea193dd0
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-04-29 22:47:31 +0900

    bootstrap.phpコメント追加

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,8 +1,18 @@
 <?php
 
+/*
+  bootstrap.php 内容
+    作成したClassLoaderをオートロードに登録するために作成
+    このbootstrqap.phpを読み込むと、オートロードが設定される
+*/
+
+// まだClassLoaderクラスは読み込まれていないので、require を使ってファイルを読み込む
 require 'core/ClassLoader.php';
 
+// インスタンス作成
 $loader = new ClassLoader();
+// coreディレクトリとmodelsディレクトリをオートロード対象ディレクトリに設定する処理
 $loader->registerDir(dirname(__File__).'/core');
 $loader->registerDir(dirname(__File__).'/models');
+// registerメソッドでオートロードに登録
 $loader->register();
