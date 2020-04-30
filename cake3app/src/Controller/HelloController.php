51ce750983726a0ef7763ac180f39ba4f7c19d95
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-04-30 22:14:23 +0900

    ビューテンプレートを作る

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 class HelloController extends AppController
 {
   public $name = 'Hello';
-  public $autoRender = false;
+  public $autoRender = true;
   
   public function index()
   {
-    $this->setAction("other"); // フォワード
+    $this->viewBuilder()->autoLayout(false);
+
+    // $this->setAction("other"); // フォワード
     // $this->redirect("/hello/other"); // リダイレクト
   }
 
-  public function other()
-  {
-    echo "これは、index以外の表示です。";
-  }
+  // public function other()
+  // {
+  //   echo "これは、index以外の表示です。";
+  // }
 }
