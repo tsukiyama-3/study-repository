63423e0072fbdd328829b6fcf9cd08f70f7e06ad
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-01 20:32:08 +0900

    新フォルダに旧フォルダの内容を上書き

---
+++ cake3app/src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+class HelloController extends AppController
+{
+  public $name = 'Hello';
+  public $autoRender = true;
+  
+  public function index()
+  {
+    $this->viewBuilder()->autoLayout(false);
+
+    // $this->setAction("other"); // フォワード
+    // $this->redirect("/hello/other"); // リダイレクト
+  }
+
+  // public function other()
+  // {
+  //   echo "これは、index以外の表示です。";
+  // }
+}
