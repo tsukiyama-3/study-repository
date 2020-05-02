805b21e3a598554d30088313d5c812a5c0e8967d
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-02 15:16:40 +0900

    フォームの作成

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -5,18 +5,21 @@ class HelloController extends AppController
 {
   public function initialize()
   {
-    $name = 'Hello';
+    // $name = 'Hello';
     // $autoRender = false;
-    $this->viewBuilder()->autoLayout(true);
+    // $this->viewBuilder()->autoLayout(true);
     $this->viewBuilder()->layout('Hello');
+
+    $this->set('msg', 'Hello/index');
+    $this->set('footer', 'Hello/footer2');
   }
   
   public function index()
   {
 
-    $this->set('msg', 'ヘッダーエレメント！！');
-    $n = rand(1,2);
-    $this->set('footer', 'Hello/footer' . $n);
+    // $this->set('msg', 'ヘッダーエレメント！！');
+    // $n = rand(1,2);
+    // $this->set('footer', 'Hello/footer' . $n);
 
     // $this->viewBuilder()->autoLayout(true);
     // $this->autoRender = true;
@@ -28,9 +31,21 @@ class HelloController extends AppController
     // $this->redirect("/hello/other"); // リダイレクト
   }
 
-  public function other()
+  public function sendForm()
   {
-    $this->viewBuilder()->autoLayout(false);
-    $this->autoRender = true;
+      $str = $this->request->query['text1'];
+      $result = "";
+      if ($str != "") {
+          $result = "you type: " . $str;
+      } else {
+          $result = "empty.";
+      }
+      $this->set("result", $result);
   }
+
+//   public function other()
+//   {
+//     $this->viewBuilder()->autoLayout(false);
+//     $this->autoRender = true;
+//   }
 }
--- cake3app/src/Template/Hello/index.ctp
+++ cake3app/src/Template/Hello/index.ctp
@@ -1,4 +1,6 @@
 <h1>サンプル見出し</h1>
-<p>こんにちは！これは、CakePHPのサンプルです。</p>
-</br></br>
-<?= date('Y/m/d', time()) ?>
+<p>フォームの送信</p>
+<form method="get" action="../hello/sendForm">
+    <input type="text" name="text1">
+    <input type="submit">
+</form>
