a304b7cf3968d31c42a7ccf4b63e53c7edbe07db
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-09 20:18:18 +0900

    レコードを更新する

--- cake3app/src/Controller/BoardsController.php
+++ cake3app/src/Controller/BoardsController.php
@@ -6,15 +6,18 @@ use \Cake\Log\Log;
 
 class BoardsController extends AppController
 {
-    public function index()
+    public function index($id = null)
     {
         $this->set('entity', $this->Boards->newEntity());
-        if ($this->request->is('post')) {
-            $id = $this->request->data['id'];
-            $data = $this->Boards->findByIdOrName($id, $id);
-        } else {
-            $data = $this->Boards->find('all');
+        if ($id != null) {
+            try {
+                $entity = $this->Boards->get($id);
+                $this->set('entity', $entity);
+            } catch(Exception $e) {
+                Log::write('debug', $e->getMessage());
+            }
         }
+        $data = $this->Boards->find('all')->order(['id' => 'DESC']);
         $this->set('data', $data->toArray());
         $this->set('count', $data->count());
     }
@@ -40,6 +43,20 @@ class BoardsController extends AppController
             );
         }
         $this->redirect(['action' => 'index']);
+    }
+
+    public function editRecord()
+    {
+        if ($this->request->is('put')) {
+            try {
+                $entity = $this->Boards->get($this->request->data['id']);
+                $this->Boards->patchEntity($entity, $this->request->data);
+                $this->Boards->save($entity);
+            } catch(Exception $e) {
+                Log::write('debug', $e->getMessage());
+            }
+        }
+        return $this->redirect(['action' => 'index']);
     }
 
 }
--- cake3app/src/Template/Boards/index.ctp
+++ cake3app/src/Template/Boards/index.ctp
@@ -1,7 +1,11 @@
 <h1>Databaseサンプル</h1>
-<?= $this->Form->create($entity, ['url' => ['action' =>'delRecord']]) ?>
+<?= $this->Form->create($entity, ['url' => ['action' =>'editRecord']]) ?>
 <fieldset>
-    <?= $this->Form->text('name') ?>
+    <p><?= 'ID = ' . $entity->id ?></p>
+    <?= $this->Form->hidden("id") ?>
+    <?= $this->Form->text("name") ?>
+    <?= $this->Form->text("title") ?>
+    <?= $this->Form->textarea("content") ?>
 </fieldset>
 <?= $this->Form->button("送信") ?>
 <?= $this->Form->end() ?>
