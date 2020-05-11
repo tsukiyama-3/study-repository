d7f1ef7fb3fbc12a936c18556be8f1eee24867ce
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-11 15:52:19 +0900

    検索条件を設定する

--- cake3app/src/Controller/BoardsController.php
+++ cake3app/src/Controller/BoardsController.php
@@ -9,6 +9,10 @@ class BoardsController extends AppController
     public function index($id = null)
     {
         $data = $this->Boards->find();
+        if ($this->request->is('post')) {
+            $input = $this->request->data['input'];
+            $data = $this->Boards->find()->where(['name' => $input]);
+        }
         $this->set('data', $data);
         $this->set('entity', $this->Boards->newEntity());
     }
