5b303e5c227d59a8219a1c9cc85c1a0645b8ce61
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-12 22:49:38 +0900

    エラ〜メッセージを用意する

--- cake3app/src/Model/Table/BoardsTable.php
+++ cake3app/src/Model/Table/BoardsTable.php
@@ -10,10 +10,17 @@ class BoardsTable extends Table
 {
     public function validationDefault(Validator $validator)
     {
-        $validator->integer('id');
-        $validator->notEmpty('name');
-        $validator->notEmpty('title');
-        $validator->notEmpty('content');
+        $validator
+            ->integer('id');
+        $validator
+            ->notEmpty('name')
+            ->minLength('name', 3, '3文字以上入力してください。')
+            ->maxLength('name', 20, '20文字以下で入力してください。');
+        $validator
+            ->notEmpty('title');
+        $validator
+            ->notEmpty('content');
+
         return $validator;
     }
 }
