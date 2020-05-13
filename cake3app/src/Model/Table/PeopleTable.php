9745f370e49fe28e020f50c3868be547a426c96a
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-14 00:05:16 +0900

    Personクラスの作成

---
+++ cake3app/src/Model/Table/PeopleTable.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class PeopleTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->hasOne('Boards');
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator->integer('id');
+        $validator->notEmpty('name', '必須項目です。');
+        $validator->notEmpty('password', '必須項目です。');
+        $validator->notEmpty('comment', '必須項目です。');
+
+        return $validator;
+    }
+
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->isUnique(['name'], '既に登録済みです。');
+    }
+}
