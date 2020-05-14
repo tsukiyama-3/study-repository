00b5e1f2af38763009fe2f427156d2c25f7ee87c
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-14 22:45:21 +0900

    ダイナミックなバインド

--- cake3app/src/Controller/PeopleController.php
+++ cake3app/src/Controller/PeopleController.php
@@ -5,6 +5,7 @@ class PeopleController extends AppController
 {
     public function index($id = null)
     {
+        $this->People->hasMany('Boards');
         $data = $this->People
             ->find('all')
             ->contain(['Boards']);
--- cake3app/src/Model/Table/PeopleTable.php
+++ cake3app/src/Model/Table/PeopleTable.php
@@ -10,7 +10,7 @@ class PeopleTable extends Table
 {
     public function initialize(array $config)
     {
-        $this->hasMany('Boards');
+        // $this->hasMany('Boards');
     }
 
     public function validationDefault(Validator $validator)
