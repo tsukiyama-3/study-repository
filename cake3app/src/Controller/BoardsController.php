cdde8e260bc1f793995655c724a2401e3fdbb189
“tsukiyama-3” <kohei.tsukiyama@yourmystar.jp>
2020-05-19 02:27:40 +0900

    コントローラーとビューの作成

--- cake3app/src/Controller/BoardsController.php
+++ cake3app/src/Controller/BoardsController.php
@@ -1,57 +1,25 @@
 <?php
 namespace App\Controller;
 
-use \Exception;
-use \Cake\Log\Log;
-use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 
 class BoardsController extends AppController
 {
-    public function index($id = null)
+    private $people;
+
+    public function initialize()
     {
-        $data = $this->Boards
+        parent::initialize();
+        $this->people = TableRegistry::get('People');
+    }
+
+    public function index()
+    {
+        $data - $this->Boards
             ->find('all')
+            ->order(['Boards.id' => 'DESC'])
             ->contain(['People']);
-        $this->set('data', $data);
+            $this->set('data', $data);
     }
-
-    public function addRecord()
-    {
-        if ($this->request->is('post')) {
-            $board = $this->Boards->newEntity($this->request->data);
-            $validator = new Validator();
-            $validator->email('name');
-            $errors = $validator->errors($this->request->data);
-            if (!empty($errors)) {
-                $this->Flash->error('EMAIL ERROR!!');
-            } else {
-                if ($this->Boards->save($board)) {
-                    $this->redirect(['action' => 'index']);
-                }
-            }
-            $this->set('entity', $board);
-        }
-    }
-
-    public function delRecord()
-    {
-        if ($this->request->is('post')) {
-            $this->Boards->deleteAll(
-                ['name'=>$this->request->data['name']]
-            );
-        }
-        $this->redirect(['action' => 'index']);
-    }
-
-    public function editRecord()
-    {
-        if ($this->request->is('post')) {
-            $arr1 = ['name' => $this->request->data['name']];
-            $arr2 = ['title' => $this->request->data['title']];
-            $this->Boards->updateAll($arr2, $arr1);
-        }
-        return $this->redirect(['action' => 'index']);
-    }
-
 }
--- cake3app/src/Template/Boards/index.ctp
+++ cake3app/src/Template/Boards/index.ctp
@@ -1,4 +1,24 @@
-<h1>Boardサンプル</h1>
-<?php foreach($data as $obj): ?>
-    <pre><?php print_r($obj->toArray()) ?></pre>
-<?php endforeach; ?>
+<h1>Boardsサンプル</h1>
+<p><?= $this->Html->link(
+    '※投稿する',
+    ['action' => 'add']
+) ?></p>
+<div>
+    <table>
+        <tr>
+            <th width="25%">投稿者</th><th>タイトル</th>
+        </tr>
+        <?php foreach ($data as $obj): ?>
+            <tr>
+                <td><?= $this->Html->link(
+                    $obj['person']['name'],
+                    ['action' => 'show2', $obj['person_id']]
+                ) ?></td>
+                <td><?= $this->Html->link(
+                    $obj['title'],
+                    ['action' => 'show', $obj['id']]
+                ) ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
